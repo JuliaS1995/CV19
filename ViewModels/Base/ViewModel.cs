a7827078c930098f931346da73c7b380e8eabb44
JS <j.sidorova1995@gmail.com>
2022-11-28 21:37:54 +0500

    Базовый класс модели представления

---
+++ ViewModels/Base/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CV19.ViewModels.Base
+{
+    internal abstract class ViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        protected virtual bool 
+    }
+}
