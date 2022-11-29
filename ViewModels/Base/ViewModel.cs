d1d2de3dd2ec8c8e4eae744129994c71c5942702
JS <j.sidorova1995@gmail.com>
2022-11-29 14:06:12 +0500

    Добавляю интерфейс iDisposable к ViewModel

--- ViewModels/Base/ViewModel.cs
+++ ViewModels/Base/ViewModel.cs
@@ -1,10 +1,11 @@
 ﻿
+using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace CV19.ViewModels.Base
 {
-    internal abstract class ViewModel:INotifyPropertyChanged
+    internal abstract class ViewModel:INotifyPropertyChanged,IDisposable //класс, который обеспечивает взаимодействие с вью модел, при измении свойств в одном месте, меняет в другом 
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -18,6 +19,17 @@ namespace CV19.ViewModels.Base
             field = value;
             OnPropertyChanged(PropertyName);
             return true;
+        }
+        public void Dispose()
+        {
+
+        }
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+            // освобождение управляемых ресурсов
         }
     }
 }
